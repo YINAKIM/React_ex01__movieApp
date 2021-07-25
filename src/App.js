b8a58519545a5f83dfda64c658efd7a7a54b7b2c
YINAKIM <yinaofficeg@gmail.com>
2021-07-25 16:48:01 +0900

    react기본2, JSX로 컴포넌트 만들고 컴포넌트 여러개 렌더링하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Potato from "./Potato";
 
 function app() {
   return(
       <div>
-        <h1> App() 컴포넌트! </h1>
+        <h1> App() 컴포넌트! Potato추가</h1>
+          <Potato />
       </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,32 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-    <App />, //import받은 바로 그 컴포넌트
-    document.getElementById('root') //이위치에 그려리
+    <App />, // Compiled successfully!
+    document.getElementById('root')
 );
+
+
+/******************************************************************************************
+ <App /><Potato />, // ERROR :  Adjacent JSX elements must be wrapped in an enclosing tag.
+ **** React는 최종으로 단 한개의 컴포넌트를 그려야 한다 ****
+
+ 그럼 Potato를 렌더링하려면?
+ react가 렌더링하는 단하나의 컴포넌트가 작성된 function의 return(); 안에다 <Potato /> 로 추가해넣으면 된다
+ 왜냐면 리액트가 렌더링하는 컴포넌트가 정의된 function에 있는 return~~~; 이부분은 JSX니까
+
+App.js에 import Potato~ 하고 이렇게 추가해넣으면
+
+ return(
+     <div>
+     <h1> App() 컴포넌트! Potato추가</h1>
+     <Potato />
+     </div>
+ );
+
+ => 결과 렌더링
+ App() 컴포넌트! Potato추가
+ I love potato, this is Potato Component with JSX
+ *******************************************************************************************/
+
+
 
