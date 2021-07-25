a31ab247ce260836900f920e35fa185353c57bed
YINAKIM <yinaofficeg@gmail.com>
2021-07-25 23:54:52 +0900

    반환되는 컴포넌트 각 요소 별로 유일한 key props 추가하기(에러메세지 해결하기 : Warning: Each child in a list should have a unique key prop. 해결하기)

--- src/App.js
+++ src/App.js
@@ -12,44 +12,42 @@ function Food({name, picture}) {
 }
 
 
-// 서버에서 데이터가 넘어온다고 가정, 변수하나 선언하고 빈 배열 할당해서 {name:'', image:''}가 들어있는 배열을 만든다, 이미지주소는 웹에서 복사해옴
+// 각 요소에 유일한 key값 넣어주기
 const foodLike = [
-    {name:'salad', image:'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'}
-    ,{name:'toast', image:'https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80'}
-    ,{name:'icecream', image:'https://images.unsplash.com/photo-1497034825429-c343d7c6a68f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fGZvb2R8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',}
-    ,{name:'fruits', image:'https://images.unsplash.com/photo-1511688878353-3a2f5be94cd7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzB8fGZvb2R8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',}
+    {id:1, name:'salad', image:'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'}
+    ,{id:2, name:'toast', image:'https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80'}
+    ,{id:3, name:'icecream', image:'https://images.unsplash.com/photo-1497034825429-c343d7c6a68f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fGZvb2R8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',}
+    ,{id:4, name:'fruits', image:'https://images.unsplash.com/photo-1511688878353-3a2f5be94cd7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzB8fGZvb2R8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',}
 ];
 
-// foodLike.map에 인자로 전달될 함수 : 음식요소를 렌더링하는 컴포넌트를 반환한다.
-function renderFood(dish) {
-    return <Food name={dish.name} picture={dish.image}/>;
-}
-
 function App() {
-    console.log(foodLike.map(renderFood));
-    /*******************************************************************************************************************************
+    // key값추가된거 확인해보기
+    console.log(foodLike.map(dish=>(
+            <Food key={dish.id} name={dish.name} picture={dish.image} />
+        )
+    ));
+    /*****************************************************
+     * 이 전에 id가 없었을 때는 key:null이라서
+     * Warning: Each child in a list should have a unique "key" prop.
+     * 이런에러메세지가 떴음
+     *
      (4) [{…}, {…}, {…}, {…}]
-    여기서 0번째 요소만 예를 들어 살펴보면
-    ---> react.element고 props에 내가 정의해둔 데이터가 전달된 것이 보인다.
-    ---> type은 Food컴포넌트타입
+     0: {$$typeof: Symbol(react.element), key: "1", ref: null, props: {…}, type: ƒ, …}
+     1: {$$typeof: Symbol(react.element), key: "2", ref: null, props: {…}, type: ƒ, …}
+     2: {$$typeof: Symbol(react.element), key: "3", ref: null, props: {…}, type: ƒ, …}
+     3: {$$typeof: Symbol(react.element), key: "4", ref: null, props: {…}, type: ƒ, …}
 
-    0:
-    $$typeof: Symbol(react.element)
-    key: null
-    props: {name: "salad", picture: "https://images.unsplash.com/photo-1512621776951-a5…8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"}
-    ref: null
-    type: ƒ Food({ name, picture })
-    _owner: FiberNode {tag: 0, key: null, stateNode: null, elementType: ƒ, type: ƒ, …}
-    _store: {validated: false}
-    _self: undefined
-    _source: {fileName: "/Users/kim-yina/Desktop/yinaLab/React_ex/react_ex01_movie/src/App.js", lineNumber: 25, columnNumber: 12}
-    [[Prototype]]: Object
-    *********************************************************************************************************************************/
+     이제 각 요소별로 유일한 key값이 있어서 에러메세지 안뜬다. 
+     ***************************************************/
     return(
         <div>
-            <h2> map() 에 인자로 Food컴포넌트에 props를 받아 컴포넌트를 반환하는 renderFood()를 받아 실행시킨다</h2>
+            <h1> map() 사용하여 비슷한 컴포넌트 여러개 만들기</h1>
           <hr/>
-            {foodLike.map(renderFood)}
+            {/* map() 에 renderFood를 인자로 받아 실행시킨다.*/}
+            {foodLike.map(dish=>(
+                <Food key={dish.id} name={dish.name} picture={dish.image} />
+                )
+             )}
       </div>
   );
 }
