a46fa8b3da8d884e28a049511d20e8b5f87a14bf
YINAKIM <yinaofficeg@gmail.com>
2021-07-25 23:23:06 +0900

    map() 사용하여 배열요소마다 컴포넌트를 반환하도록: 서버에서 데이터를 받아올 때 사용하는 방법

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
 import React from 'react';
 
-function Food(props) {
-     console.log(props); // {fav: "Kimchi", something: true, papapapa: Array(6)} 가 넘어온다
-     // props를 인자로 받는 컴포넌트를 React가 렌더링하는 컴포넌트에 넣었다는 것은 ---> props를 속성으로 가지는 Object가 출력되는 것
-
-     return <h4>Props나와라 => {props.fav}</h4> ; // Props나와라 => Kimchi , 근데 true, false값은 안나온다(props.something은 암것도 출력안됨)
+function Food({name, picture}) {
+    return(
+        <div>
+            <h4>이름 : {name}</h4>
+            <img src ={picture} alt={name} width="200px"/>
+            <br/>
+            <br/>
+        </div>
+    );
 }
 
-function Food2({fav}) {
-     console.log({fav}); // {fav: "Kimchi", something: true, papapapa: Array(6)} 가 넘어온다
-     // props를 인자로 받는 컴포넌트를 React가 렌더링하는 컴포넌트에 넣었다는 것은 ---> props를 속성으로 가지는 Object가 출력되는 것
 
-     return <h4>Props나와라 => {fav}</h4> ; // Props나와라 => Kimchi , 근데 true, false값은 안나온다(props.something은 암것도 출력안됨)
+// 서버에서 데이터가 넘어온다고 가정, 변수하나 선언하고 빈 배열 할당해서 {name:'', image:''}가 들어있는 배열을 만든다, 이미지주소는 웹에서 복사해옴
+const foodLike = [
+    {name:'salad', image:'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'}
+    ,{name:'toast', image:'https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80'}
+    ,{name:'icecream', image:'https://images.unsplash.com/photo-1497034825429-c343d7c6a68f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fGZvb2R8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',}
+    ,{name:'fruits', image:'https://images.unsplash.com/photo-1511688878353-3a2f5be94cd7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzB8fGZvb2R8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',}
+];
+// 각 {}의 마지막요소 뒤에 ,가 있던 없던 OK
 
-    // 참고, ES6 구조분해할당 문법 : 책에서는 76p 에서 이렇게 써도 된다는데 syntax error -> 사용법 알아보고 다시 해보기
-    // { fav } = props;
-    // return <h1>{fav}</h1>;
-}
+
+
 
 function App() {
-  return(
-      <div>
-        <h1> App() 컴포넌트! Food 추가 - 여러개의 컴포넌트에서 props사용하기</h1>
-        <Food fav="Kimchi" something={false} papapapa={['hello',1,2,3,4,false]}/>
+    return(
+        <div>
+            <h1> map() 사용하여 비슷한 컴포넌트 여러개 만들기</h1>
           <hr/>
-          <Food2 fav="kimchi"/>
-          <Food2 fav="rice"/>
-          <Food2 fav="sea weed"/>
+            {/* map() 사용하여 요소마다 컴포넌트를 반환하도록*/}
+            {foodLike.map(dish => (
+                <Food name={dish.name} picture={dish.image}/>
+            ))}
       </div>
   );
 }
