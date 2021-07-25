b0ec15cb63fe6885c1041ad5c17daaa099ee355a
YINAKIM <yinaofficeg@gmail.com>
2021-07-25 15:40:52 +0900

    react 렌더링 원리

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
 function App() {
-  return(<div className='App'/>);
-  // return 이랑 같은 라인에 써야 return을 인식한다. ----> ERROR:return null
-  // 여러줄 리턴하려면? return( ); 이렇게 하고 ()안에 쓰면 여러줄 리턴해도 에러 안남
+  return <h2>Hello! 여기에 들어간다</h2>;
 }
 export default App;
